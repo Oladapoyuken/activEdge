2aa577c6008f9ad982c084bb3141c9a1fa9f533d
Oladapo Yusuf <info@nexttechnigeria.com>
2021-10-13 19:35:47 +0100

    Corrected execise 2 for sql structured statement

--- src/main/java/com/example/demo/activEdgeAssessment/execise_two.java
+++ src/main/java/com/example/demo/activEdgeAssessment/execise_two.java
@@ -2,9 +2,10 @@ package com.example.demo.activEdgeAssessment;
 
 public class execise_two {
 
+
     String sql = "" +
-            "SELECT * FROM `t2` " +
-            "WHERE `name` NOT IN " +
-            "(SELECT `name` FROM `t1`) " +
-            "ORDER BY `id` DESC;";
+            "SELECT * FROM T1 " +
+            "WHERE name NOT IN " +
+            "(SELECT name FROM T2) " +
+            "ORDER BY id DESC";
 }
